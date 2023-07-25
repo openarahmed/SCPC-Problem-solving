89894d3ac64ee53aadd0de95bf9be141d078eb02
Shahidul Islam <shahidulislam@Konas-iMac.local>
2023-07-25 19:59:33 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function reverseString(inputString) {
+  let reversedString = "";
+  for (let i = inputString.length - 1; i >= 0; i--) {
+    reversedString += inputString[i];
+  }
+  return reversedString;
+}
+const inputStr = "hello world";
+const outputStr = reverseString(inputStr);
